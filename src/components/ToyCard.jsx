aed3693d6c498fdc9500194e73d92f0f7b64990d
Lisa Marie <73184313+lisamcgerr@users.noreply.github.com>
2021-03-02 10:42:40 -0600

    Update ToyCard.jsx

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -2,14 +2,55 @@ import React, { Component } from 'react';
 
 class ToyCard extends Component {
 
+  handleRemove = () => {
+    const {id} = this.props.toy
+
+    fetch(`http://localhost:3000/toys/${id}`, {method: 'DELETE'})
+    .then(resp => resp.json())
+    .then(data => {
+      this.props.deleteToy(id)
+    })
+  }
+
+  updateLikes = (e) => {
+    //updating the likes using a PATCH fetch request, calling parseInt on the innerText of the node above it
+    //parseInt removing any additional string - turning to a number
+    //this.props.updateLikes(updatedToy) to update the state of the toy's likes in App.js so that it can be 
+    //rendered in the front end
+    //console.log(e.target)
+    const {id} = this.props.toy
+
+    const likes = parseInt(e.target.previousElementSibling.innerText)
+
+    const reqObj = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        likes: likes + 1
+      })
+    }
+
+    fetch(`http://localhost:3000/toys/${id}`, reqObj)
+    .then(resp => resp.json())
+    .then(updatedToy => {
+      this.props.updateLikes(updatedToy)
+    })
+    
+  }
+
   render() {
+
+    const { name, image, likes } = this.props.toy
+
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={'' /* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{name}</h2>
+        <img src={image} alt={name} className="toy-avatar" />
+        <p>{likes} Likes </p>
+        <button onClick={this.updateLikes} className="like-btn">Like {'<3'}</button>
+        <button onClick={this.handleRemove} className="del-btn">Donate to GoodWill</button>
       </div>
     );
   }
